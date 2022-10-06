fa59887774aea6e29b6299dd7358e1c97edc9646
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2022-10-06 22:19:02 +0300

    Bump version -> `2.0.0-SNAPSHOT.30`
    Also:
     * Bump `mc-java` -> `2.0.0-SNAPSHOT.100`

--- version.gradle.kts
+++ version.gradle.kts
@@ -29,6 +29,6 @@ val spineToolBaseVersion by extra("2.0.0-SNAPSHOT.95")
 val spineTimeVersion by extra("2.0.0-SNAPSHOT.96")
 val spineServerVersion by extra("2.0.0-SNAPSHOT.108")
 val protoDataVersion by extra("0.2.16")
-val mcJavaVersion by extra("2.0.0-SNAPSHOT.99")
+val mcJavaVersion by extra("2.0.0-SNAPSHOT.100")
 
-val validationVersion by extra("2.0.0-SNAPSHOT.29")
+val validationVersion by extra("2.0.0-SNAPSHOT.30")
