a54619057b9d882414fade2b9abced0eead00984
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2023-02-24 13:09:09 +0000

    Bump ProtoData -> `0.7.2`

--- buildSrc/src/main/kotlin/io/spine/internal/dependency/Spine.kt
+++ buildSrc/src/main/kotlin/io/spine/internal/dependency/Spine.kt
@@ -61,7 +61,7 @@ class Spine(p: ExtensionAware) {
          *
          * @see [ProtoData]
          */
-        const val protoData = "0.7.0"
+        const val protoData = "0.7.2"
 
         /**
          * The default version of `base` to use.
