1085d6493fa805404fb8bdb4b4f59b0882343119
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2023-05-04 11:57:33 +0300

    Bump ProtoData version

--- buildSrc/src/main/kotlin/io/spine/internal/dependency/Spine.kt
+++ buildSrc/src/main/kotlin/io/spine/internal/dependency/Spine.kt
@@ -61,7 +61,7 @@ class Spine(p: ExtensionAware) {
          *
          * @see [ProtoData]
          */
-        const val protoData = "0.8.0"
+        const val protoData = "0.8.1"
 
         /**
          * The default version of `base` to use.
