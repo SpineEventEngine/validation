4cb0b98129ff40209f051c2d64e414c2acebc9a5
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2023-05-05 12:27:42 +0300

    Bump mc-java version

--- buildSrc/src/main/kotlin/io/spine/internal/dependency/Spine.kt
+++ buildSrc/src/main/kotlin/io/spine/internal/dependency/Spine.kt
@@ -85,7 +85,7 @@ class Spine(p: ExtensionAware) {
         /**
          * The version of `mc-java` to use.
          */
-        const val mcJava = "2.0.0-SNAPSHOT.145"
+        const val mcJava = "2.0.0-SNAPSHOT.146"
 
         /**
          * The version of `base-types` to use.
