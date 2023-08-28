373e248d675e11b6f52e92c64b171c788d056756
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2023-08-28 17:00:54 +0300

    Better doc and full delegation for JavaValidationPlugin

--- java/src/main/java/io/spine/validation/java/JavaValidationPlugin.java
+++ java/src/main/java/io/spine/validation/java/JavaValidationPlugin.java
@@ -29,8 +29,11 @@ package io.spine.validation.java;
 import com.google.common.collect.ImmutableList;
 import io.spine.protodata.plugin.Plugin;
 import io.spine.protodata.plugin.Policy;
+import io.spine.protodata.plugin.View;
 import io.spine.protodata.plugin.ViewRepository;
 import io.spine.protodata.renderer.Renderer;
+import io.spine.protodata.type.TypeConvention;
+import io.spine.server.BoundedContextBuilder;
 import io.spine.validation.ValidationPlugin;
 
 import java.util.List;
@@ -40,18 +43,63 @@ import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  * A plugin that sets up everything needed to generate Java validation code.
+ *
+ * <p>This plugin uses a delegate plugin to set up some on the components needed for
+ * code generation. By default, a {@link ValidationPlugin} is used. However, API users may
+ * extend this plugin's behavior and supply a more rich base plugin.
  */
 @SuppressWarnings("unused") // Accessed via reflection.
 public final class JavaValidationPlugin implements Plugin {
 
     private final Plugin base;
 
+    /**
+     * Constructs a {@code JavaValidationPlugin} that contains all the components from
+     * the given {@code base} plugin.
+     *
+     * @param base the base plugin to extend
+     */
     public JavaValidationPlugin(Plugin base) {
         this.base = checkNotNull(base);
     }
 
+    /**
+     * Constructs a {@code JavaValidationPlugin} based on a {@link ValidationPlugin}.
+     *
+     * <p>This is the default constructor used by ProtoData's reflective mechanisms.
+     */
     public JavaValidationPlugin() {
         this(new ValidationPlugin());
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>{@code JavaValidationPlugin} orders the renderers in such a way that the renderers of
+     * the {@code base} plugin always come before its own renderers.
+     */
+    @Override
+    public List<Renderer<?>> renderers() {
+        var result = ImmutableList.<Renderer<?>>builder();
+        result.addAll(base.renderers());
+        result.add(new PrintValidationInsertionPoints(),
+                   new JavaValidationRenderer());
+        return result.build();
+    }
+
+    @Override
+    public void extend(BoundedContextBuilder context) {
+        base.extend(context);
+    }
+
+    @Override
+    public Set<TypeConvention<?, ?>> typeConventions() {
+        return base.typeConventions();
+    }
+
+    @Override
+    public Set<Class<? extends View<?, ?, ?>>> views() {
+        return base.views();
     }
 
     @Override
@@ -62,13 +110,5 @@ public final class JavaValidationPlugin implements Plugin {
     @Override
     public Set<ViewRepository<?, ?, ?>> viewRepositories() {
         return base.viewRepositories();
-    }
-
-    @Override
-    public List<Renderer<?>> renderers() {
-        return ImmutableList.of(
-                new PrintValidationInsertionPoints(),
-                new JavaValidationRenderer()
-        );
     }
 }
