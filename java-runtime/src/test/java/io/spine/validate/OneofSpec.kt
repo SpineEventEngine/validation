5568d473dd40bf4494cefc5d85252a14559e1616
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2023-11-09 06:15:02 +0000

    Сonvert tests to Kotlin

--- java-runtime/src/test/java/io/spine/validate/OneofSpec.kt
+++ java-runtime/src/test/java/io/spine/validate/OneofSpec.kt
@@ -23,135 +23,112 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+package io.spine.validate
 
-package io.spine.validate;
-
-import io.spine.test.validate.oneof.OneofAndOtherAreRequired;
-import io.spine.test.validate.oneof.OneofWithOptionalFields;
-import io.spine.test.validate.oneof.OneofWithRequiredFields;
-import io.spine.test.validate.oneof.OneofWithValidation;
-import io.spine.test.validate.oneof.RequiredOneofWithValidation;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
-
-import static io.spine.base.Identifier.newUuid;
-import static io.spine.validate.ValidationOfConstraintTest.VALIDATION_SHOULD;
+import io.spine.base.Identifier
+import io.spine.test.validate.oneof.OneofAndOtherAreRequired
+import io.spine.test.validate.oneof.OneofWithOptionalFields
+import io.spine.test.validate.oneof.OneofWithRequiredFields
+import io.spine.test.validate.oneof.OneofWithValidation
+import io.spine.test.validate.oneof.RequiredOneofWithValidation
+import io.spine.validate.ValidationOfConstraintTest.Companion.VALIDATION_SHOULD
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
 
 @DisplayName(VALIDATION_SHOULD + "consider `oneof`")
-final class OneofTest extends ValidationOfConstraintTest {
+internal class OneofSpec : ValidationOfConstraintTest() {
 
     @DisplayName("valid if")
     @Nested
-    final class Valid {
+    internal inner class Valid {
 
         @Test
-        @DisplayName("a required field is set to a non-default value")
-        void requiredFieldIsNotDefault() {
-            var requiredIsSet = OneofWithRequiredFields.newBuilder()
-                    .setFirst(newUuid())
-                    .build();
-            assertValid(requiredIsSet);
+        fun `a required field is set to a non-default value`() = assertValid {
+            OneofWithRequiredFields.newBuilder()
+                .setFirst(Identifier.newUuid())
+                .build()
         }
 
         @Test
-        @DisplayName("all required fields are set")
-        void allRequiredFieldsAreNotDefault() {
-            var requiredAreSet = OneofAndOtherAreRequired.newBuilder()
-                    .setSecond(newUuid())
-                    .setThird(newUuid())
-                    .build();
-            assertValid(requiredAreSet);
+        fun `all required fields are set`() = assertValid {
+            OneofAndOtherAreRequired.newBuilder()
+                .setSecond(Identifier.newUuid())
+                .setThird(Identifier.newUuid())
+                .build()
         }
 
         @Test
-        @DisplayName("an optional field is set to the default value")
-        void optionalIsDefault() {
-            var optionalIsDefault = OneofWithOptionalFields.newBuilder()
-                    .setFirst("")
-                    .build();
-            assertValid(optionalIsDefault);
-            assertValid(OneofWithOptionalFields.getDefaultInstance());
+        fun `an optional field is set to the default value`() {
+            val optionalIsDefault = OneofWithOptionalFields.newBuilder()
+                .setFirst("")
+                .build()
+            assertValid(optionalIsDefault)
+            assertValid(OneofWithOptionalFields.getDefaultInstance())
         }
 
         @Test
-        @DisplayName("an optional field is properly validated")
-        void optionalIsValid() {
-            var validFieldSet = OneofWithValidation.newBuilder()
-                    .setWithValidation("valid")
-                    .build();
-            assertValid(validFieldSet);
+        fun `an optional field is properly validated`() {
+            val validFieldSet = OneofWithValidation.newBuilder()
+                .setWithValidation("valid")
+                .build()
+            assertValid(validFieldSet)
         }
 
         @Test
-        @DisplayName("an optional validated field is is default")
-        void optionalFieldWithValidationIsDefault() {
-            assertValid(OneofWithValidation.getDefaultInstance());
+        fun `an optional validated field is is default`() =
+            assertValid(OneofWithValidation.getDefaultInstance())
+
+        @Test
+        fun `an optional field without validation is set`() = assertValid {
+            OneofWithValidation.newBuilder()
+                .setNoValidation("does not require validation")
+                .build()
         }
 
         @Test
-        @DisplayName("an optional field without validation is set")
-        void optionalFieldWithoutValidationSet() {
-            var fieldWithoutValidationSet = OneofWithValidation.newBuilder()
-                    .setNoValidation("does not require validation")
-                    .build();
-            assertValid(fieldWithoutValidationSet);
+        fun `a required field without validation is set`() = assertValid {
+            RequiredOneofWithValidation.newBuilder()
+                .setRawValue("o_0")
+                .build()
         }
 
         @Test
-        @DisplayName("a required field without validation is set")
-        void requiredNonValidatedFieldSet() {
-            var requiredWithoutValidationSet = RequiredOneofWithValidation.newBuilder()
-                    .setRawValue("o_0")
-                    .build();
-            assertValid(requiredWithoutValidationSet);
-        }
-
-        @Test
-        @DisplayName("a required field with validation is set")
-        void requiredValidatedFieldSet() {
-            var requiredWithValidationSet = RequiredOneofWithValidation.newBuilder()
-                    .setValidValue("aaa1111")
-                    .build();
-            assertValid(requiredWithValidationSet);
+        fun `a required field with validation is set`() = assertValid {
+            RequiredOneofWithValidation.newBuilder()
+                .setValidValue("aaa1111")
+                .build()
         }
     }
 
     @DisplayName("invalid if")
     @Nested
-    final class Invalid {
+    internal inner class Invalid {
 
         @Test
-        @DisplayName("a required field is set to the default value")
-        void requiredFieldIsDefault() {
-            var requiredIsDefault = OneofWithRequiredFields.newBuilder()
-                    .setFirst("")
-                    .build();
-            assertNotValid(requiredIsDefault, false);
+        fun `a required field is set to the default value`() {
+            val requiredIsDefault = OneofWithRequiredFields.newBuilder()
+                .setFirst("")
+                .build()
+            assertNotValid(requiredIsDefault, false)
         }
 
         @Test
-        @DisplayName("a field within `oneof` is not valid")
-        void fieldIsNotValid() {
-            var validFieldSet = OneofWithValidation.newBuilder()
-                    .setWithValidation("   ")
-                    .build();
-            assertNotValid(validFieldSet);
-        }
+        fun `a field within 'oneof' is not valid`() = assertNotValid(
+            OneofWithValidation.newBuilder()
+                .setWithValidation("   ")
+                .build()
+        )
 
         @Test
-        @DisplayName("a required field is not set")
-        void requiredFieldNotSet() {
-            assertNotValid(OneofWithRequiredFields.getDefaultInstance(), false);
-        }
+        fun `a required field is not set`() =
+            assertNotValid(OneofWithRequiredFields.getDefaultInstance(), false)
 
         @Test
-        @DisplayName("a required field is not valid")
-        void requiredFieldIsNotValid() {
-            var requiredWithValidationSet = RequiredOneofWithValidation.newBuilder()
-                    .setValidValue("###")
-                    .build();
-            assertNotValid(requiredWithValidationSet);
-        }
+        fun `a required field is not valid`() = assertNotValid(
+            RequiredOneofWithValidation.newBuilder()
+                .setValidValue("###")
+                .build()
+        )
     }
 }
