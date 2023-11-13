7f993db28bbe0b14e84fbea0ede3a2c8346e381e
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2023-11-13 22:50:46 +0000

    Improve generated var names

--- java/src/main/kotlin/io/spine/validation/java/DistributingGenerator.kt
+++ java/src/main/kotlin/io/spine/validation/java/DistributingGenerator.kt
@@ -30,23 +30,30 @@ import com.google.common.collect.ImmutableList
 import com.google.common.reflect.TypeToken
 import com.squareup.javapoet.CodeBlock
 import com.squareup.javapoet.MethodSpec
-import io.spine.base.Identifier.newUuid
+import io.spine.protodata.Field
 import io.spine.protodata.codegen.java.Expression
 import io.spine.protodata.codegen.java.Literal
 import io.spine.protodata.codegen.java.MethodCall
 import io.spine.protodata.codegen.java.This
 import io.spine.protodata.isMap
+import io.spine.string.titleCase
 import io.spine.validate.ConstraintViolation
 import io.spine.validation.ErrorMessage
+import java.security.SecureRandom
 import javax.lang.model.element.Modifier
+import kotlin.io.encoding.Base64
+import kotlin.io.encoding.ExperimentalEncodingApi
 
 /**
  * A code generator that distributes the associated simple validation rule to elements of
  * a repeated field.
  *
- * A rule applied to a repeated fields, i.e. a list or a map, may be applied to the fields as
- * a whole, or to individual elements of the field. In the first case, no extra modifications to
- * code generation are required. In the second case, this generator creates code that iterated over
+ * A rule applied to a repeated fields, i.e., a list or a map, may be applied to the fields as
+ * a whole, or to individual elements of the field.
+ *
+ * In the first case, no extra modifications to code generation are required.
+ *
+ * In the second case, this generator creates code that iterated over
  * the elements and applies the validation rule to them.
  */
 internal class DistributingGenerator(
@@ -58,9 +65,9 @@ internal class DistributingGenerator(
     private val elementContext = ctx.copy(elementReference = element)
     private val field = ctx.simpleRuleField
     private val delegate = delegateCtor(elementContext)
-    private val ruleId = "${field.name.value}_${newUuid().filter { it.isJavaIdentifierPart() }}"
-    private val methodName = "validate_$ruleId"
-    private val violationsName = "violations_of_$ruleId"
+    private val ruleId = field.ruleId()
+    private val methodName = "validate$ruleId"
+    private val violationsName = "violationsOf$ruleId"
     private val violationsType = object : TypeToken<ImmutableList<ConstraintViolation>>() {}.type
 
     override fun supportingMembers(): CodeBlock {
@@ -76,8 +83,7 @@ internal class DistributingGenerator(
         } else {
             fieldAccessor
         }
-        val body = CodeBlock
-            .builder()
+        val body = CodeBlock.builder()
             .addStatement(
                 "\$T<\$T> \$L = \$T.builder()",
                 ImmutableList.Builder::class.java,
@@ -125,3 +131,33 @@ internal class DistributingGenerator(
             .build()
     }
 }
+
+private const val RANDOM_STR_BYTES = 10
+
+private fun Field.ruleId(): String {
+    val random = SecureRandomString.generate(RANDOM_STR_BYTES).filter { it.isJavaIdentifierPart() }
+    val fieldRef = name.value.titleCase()
+    return fieldRef + "_" + random
+}
+
+// TODO: Migrate to SecureRandomString from ProtoData.
+@OptIn(ExperimentalEncodingApi::class)
+private object SecureRandomString {
+
+    private const val DEFAULT_SIZE = 20
+
+    private val random: SecureRandom by lazy {
+        SecureRandom()
+    }
+
+    private val encoder: Base64 by lazy {
+        Base64.UrlSafe
+    }
+
+    fun generate(size: Int = DEFAULT_SIZE): String {
+        val buffer = ByteArray(size)
+        random.nextBytes(buffer)
+        return encoder.encode(buffer)
+    }
+}
+
