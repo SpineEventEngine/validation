836ea1b615f545acb7b19f0c14b7840d5255730c
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2024-01-25 15:55:32 +0000

    Remove redundant forcing of versions

--- java-runtime/build.gradle.kts
+++ java-runtime/build.gradle.kts
@@ -41,19 +41,3 @@ dependencies {
     implementation(Spine.Logging.lib)
     testImplementation(Spine.testlib)
 }
-
-configurations.forceProtoData()
-
-
-// Uncomment the below block when remote debugging of code generation is needed.
-//
-//tasks.findByName("launchTestProtoData")?.apply {this as JavaExec
-//    debugOptions {
-//        // Set this option to `true` to enable remote debugging.
-//        enabled.set(true)
-//        port.set(5566)
-//        server.set(true)
-//        suspend.set(true)
-//    }
-//    System.err.println("Debug session for `:java-runtime test` configured.")
-//}
--- java-tests/build.gradle.kts
+++ java-tests/build.gradle.kts
@@ -75,6 +75,4 @@ subprojects {
             artifact = Protobuf.compiler
         }
     }
-
-    configurations.forceProtoData()
 }
