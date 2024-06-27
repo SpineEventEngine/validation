416a01c3eb8746bbd213c19bbbb51615a6772693
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2024-06-27 23:10:23 +0100

    Use `String` instead of `Text`

--- java/src/main/java/io/spine/validation/java/ExtraValidation.java
+++ java/src/main/java/io/spine/validation/java/ExtraValidation.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2023, TeamDev. All rights reserved.
+ * Copyright 2024, TeamDev. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Redistribution and use in source and/or binary forms, with or without
  * modification, must retain the above copyright notice and the following
@@ -28,7 +28,6 @@ package io.spine.validation.java;
 
 import io.spine.protodata.TypeName;
 import io.spine.protodata.renderer.NonRepeatingInsertionPoint;
-import io.spine.text.Text;
 import io.spine.text.TextCoordinates;
 
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -66,7 +65,7 @@ public final class ExtraValidation implements NonRepeatingInsertionPoint {
      * Thus, this method always returns {@code LineNumber.notInFile()}.
      */
     @Override
-    public TextCoordinates locateOccurrence(Text t) {
+    public TextCoordinates locateOccurrence(String code) {
         return nowhere();
     }
 }
