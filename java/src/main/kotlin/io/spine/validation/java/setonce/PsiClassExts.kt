b74ae6c89f3d780a2bc1cf91e4336aea9ad39261
yevhenii-nadtochii <yevhenii.nadtochii@teamdev.com>
2024-11-04 15:57:42 +0100

    Rephrase the error message

--- java/src/main/kotlin/io/spine/validation/java/setonce/PsiClassExts.kt
+++ java/src/main/kotlin/io/spine/validation/java/setonce/PsiClassExts.kt
@@ -50,4 +50,7 @@ internal fun PsiClass.findMethodBySignature(text: String): PsiMethod? {
  */
 internal fun PsiClass.getMethodBySignature(text: String): PsiMethod =
     findMethodBySignature(text)
-        ?: error("Could not find method `$text` in `$qualifiedName` class.")
+        ?: error(
+            "Could not find the method with the signature `$text` " +
+                    "in the `$qualifiedName` class."
+        )
