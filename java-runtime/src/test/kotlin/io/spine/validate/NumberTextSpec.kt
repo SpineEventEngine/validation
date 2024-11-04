6a5fcb2e6b0e38d82a9cdc5ac965267ec3d7762b
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2024-11-04 20:42:17 +0000

    Convert tests to Kotlin

--- java-runtime/src/test/kotlin/io/spine/validate/NumberTextSpec.kt
+++ java-runtime/src/test/kotlin/io/spine/validate/NumberTextSpec.kt
@@ -1,11 +1,11 @@
 /*
- * Copyright 2022, TeamDev. All rights reserved.
+ * Copyright 2024, TeamDev. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Redistribution and use in source and/or binary forms, with or without
  * modification, must retain the above copyright notice and the following
@@ -24,123 +24,110 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package io.spine.validate;
+package io.spine.validate
 
-import com.google.common.testing.EqualsTester;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.stream.Stream;
-
-import static io.spine.testing.Assertions.assertIllegalState;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import com.google.common.testing.EqualsTester
+import io.kotest.matchers.shouldBe
+import java.util.stream.Stream
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 @DisplayName("`NumberText` numbers should")
-class NumberTextTest {
+internal class NumberTextSpec {
 
     @Test
-    @DisplayName("have a correct equality relationship")
-    void testEquals() {
-        var equalsTester = new EqualsTester();
-        equalsTester.addEqualityGroup(new NumberText("0.0"), new NumberText("0.0"))
-                    .addEqualityGroup(new NumberText("0.1"), new NumberText("0.10"))
-                    .testEquals();
+    fun `have a correct equality relationship`() {
+        EqualsTester()
+            .addEqualityGroup(NumberText("0.0"), NumberText("0.0"))
+            .addEqualityGroup(NumberText("0.1"), NumberText("0.10"))
+            .testEquals()
     }
 
     @Test
-    @DisplayName("recognize that two numbers have different types")
-    void differentTypeTest() {
-        var plainNumber = "1";
-        var numberWithDecimalPart = "1.0";
-        var plain = new NumberText(plainNumber);
-        var withDecimal = new NumberText(numberWithDecimalPart);
-        assertFalse(plain.isOfSameType(withDecimal));
+    fun `recognize that two numbers have different types`() {
+        val plain = NumberText("1")
+        val withDecimal = NumberText("1.0")
+
+        plain.isOfSameType(withDecimal) shouldBe true
     }
 
     @Test
-    @DisplayName("recognize that two numbers are of the same types")
-    void sameTypeTest() {
-        var fitsIntoByte = "4";
-        var maxInteger = String.valueOf(Integer.MAX_VALUE);
-        var small = new NumberText(fitsIntoByte);
-        var large = new NumberText(maxInteger);
-        assertTrue(small.isOfSameType(large));
+    fun `recognize that two numbers are of the same types`() {
+        val fitsIntoByte = NumberText("4")
+        val maxInt = NumberText(Int.MAX_VALUE.toString())
+
+        fitsIntoByte.isOfSameType(maxInt) shouldBe true
     }
 
     @Test
-    @DisplayName("compare values")
-    void comparisonTest() {
-        var smallerValue = "0.1";
-        var largerValue = "15";
-        var smaller = new NumberText(smallerValue);
-        var larger = new NumberText(largerValue);
-        var comparison = smaller.toNumber()
-                                .compareTo(larger.toNumber());
-        assertTrue(comparison < 0);
+    fun `compare values`() {
+        val smaller = NumberText("0.1")
+        val larger = NumberText("15")
+        val comparison = smaller.toNumber().compareTo(larger.toNumber())
+
+        (comparison < 0) shouldBe true
     }
 
     @Test
-    @DisplayName("store numbers that do not fit into `int`")
-    void testDoesNotFitIntoInt() {
-        var longMaxValue = String.valueOf(Long.MAX_VALUE);
-        var lessThanLongMaxValue = String.valueOf(Long.MAX_VALUE - 1);
-        var larger = new NumberText(longMaxValue);
-        var smaller = new NumberText(lessThanLongMaxValue);
-        assertEquals(1, larger.toNumber()
-                              .compareTo(smaller.toNumber()));
+    fun `store numbers that do not fit into 'int'`() {
+        val longMax = NumberText(Long.MAX_VALUE.toString())
+        val lessThanLongMax = NumberText((Long.MAX_VALUE - 1).toString())
+
+        longMax.toNumber().compareTo(lessThanLongMax.toNumber()) shouldBe 1
     }
 
     @ParameterizedTest
     @MethodSource("textNumbers")
-    @DisplayName("stringify values")
-    void toStringTest(Number input, String expected) {
-        var text = new NumberText(input);
-        assertEquals(expected, text.toString());
+    fun `stringify values`(input: Number, expected: String) {
+        val text = NumberText(input)
+        text.toString() shouldBe expected
     }
 
-    @SuppressWarnings("unused") // invoked via `@MethodSource`.
-    private static Stream<Arguments> textNumbers() {
-        return Stream.of(
-                Arguments.of(0.0d, "0.0"),
-                Arguments.of(0, "0"),
-                Arguments.of(-1.0d, "-1.0"),
-                Arguments.of(-1, "-1"),
-                Arguments.of(-1.23456789d, "-1.23456789"),
-                Arguments.of(-3L, "-3"),
-                Arguments.of(-2.23456f, "-2.23456")
-        );
-    }
-
-    @DisplayName("throw on malformed numbers")
     @ParameterizedTest
     @MethodSource("malformedNumbers")
-    void throwOnMalformedNumbers(String malformed) {
-        assertThrows(NumberFormatException.class, () -> new NumberText(malformed));
+    fun `throw on malformed numbers`(malformed: String) {
+        assertThrows<NumberFormatException> {
+            NumberText(malformed)
+        }
     }
 
     @DisplayName("throw on a number with too many decimal separators")
     @Test
-    void throwOnTooManySeparators() {
-        assertIllegalState(() -> new NumberText("1.0.0"));
+    fun throwOnTooManySeparators() {
+        assertThrows<IllegalStateException> {
+            NumberText("1.0.0")
+        }
     }
 
-    @SuppressWarnings("unused") // invoked via `@MethodSource`.
-    private static Stream<Arguments> malformedNumbers() {
-        return Stream.of(
-                Arguments.of("1,0,0"),
-                Arguments.of("1,0"),
-                // Even though those are technically expressions that evaluate to a number,
-                // they are not allowed.
-                Arguments.of("2!"),
-                Arguments.of("2/2"),
-                Arguments.of("2+2"),
-                Arguments.of("2-2")
-        );
+    @Suppress("unused") // invoked via `@MethodSource`.
+    companion object {
+
+        @JvmStatic
+        fun textNumbers(): Stream<Arguments> = Stream.of(
+            Arguments.of(0.0, "0.0"),
+            Arguments.of(0, "0"),
+            Arguments.of(-1.0, "-1.0"),
+            Arguments.of(-1, "-1"),
+            Arguments.of(-1.23456789, "-1.23456789"),
+            Arguments.of(-3L, "-3"),
+            Arguments.of(-2.23456f, "-2.23456")
+        )
+
+        @JvmStatic
+        fun malformedNumbers(): Stream<Arguments> = Stream.of(
+            Arguments.of("1,0,0"),
+            /* Even though the expressions below technically evaluate to a number,
+               they are not allowed because we do not want to build and support
+               a calculator on Protobuf options. */
+            Arguments.of("1,0"),
+            Arguments.of("2!"),
+            Arguments.of("2/2"),
+            Arguments.of("2+2"),
+            Arguments.of("2-2")
+        )
     }
 }
