6f448eb5b122d51839fe23ba25190fb2191a0f6d
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2024-11-06 13:18:36 +0000

    Migrate tests to Kotlin

--- java-tests/validating/src/test/kotlin/io/spine/test/options/ValidateConstraintTest.kt
+++ java-tests/validating/src/test/kotlin/io/spine/test/options/ValidateConstraintTest.kt
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Redistribution and use in source and/or binary forms, with or without
  * modification, must retain the above copyright notice and the following
@@ -24,118 +24,134 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package io.spine.test.tools.validate;
+package io.spine.test.options
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import static com.google.common.truth.Truth.assertThat;
-import static com.google.common.truth.Truth8.assertThat;
-import static io.spine.base.Identifier.newUuid;
-import static io.spine.base.Identifier.pack;
+import io.kotest.matchers.optional.shouldBeEmpty
+import io.kotest.matchers.optional.shouldBePresent
+import io.kotest.matchers.shouldBe
+import io.spine.base.Identifier
+import io.spine.test.tools.validate.Address
+import io.spine.test.tools.validate.BinaryTree
+import io.spine.test.tools.validate.DeliveryReceiver
+import io.spine.test.tools.validate.EmailAddress
+import io.spine.test.tools.validate.PersonName
+import io.spine.test.tools.validate.PhoneNumber
+import io.spine.test.tools.validate.Town
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 
 @DisplayName("`(validate)` constraint should be compiled so that")
-class ValidateConstraintTest {
+internal class ValidateConstraintTest {
 
     @Test
-    @DisplayName("message fields are validated")
-    void checkEnclosedFields() {
-        var wrongAddress = DeliveryReceiver.newBuilder()
-                .setName(PersonName.newBuilder()
-                                 .setGivenName("Adam")
-                                 .buildPartial())
-                .setAddress(Address.newBuilder()
-                                    .setSecondLine("Wall St. 1")
-                                    .buildPartial())
-                .buildPartial();
-        var error = wrongAddress.validate();
-        assertThat(error)
-                .isPresent();
-        var violations = error.get()
-                              .getConstraintViolationList();
-        assertThat(violations)
-                .hasSize(1);
-        var wrapperViolation = violations.get(0);
-        assertThat(wrapperViolation.getFieldPath()
-                                   .getFieldName(0))
-                .isEqualTo("address");
-        var nestedViolations = wrapperViolation.getViolationList();
-        assertThat(nestedViolations)
-                .hasSize(2);
-        assertThat(nestedViolations.get(0)
-                                   .getFieldPath()
-                                   .getFieldName(0))
-                .isEqualTo("first_line");
-        assertThat(nestedViolations.get(1)
-                                   .getFieldPath()
-                                   .getFieldName(0))
-                .isEqualTo("town");
+    fun `message fields are validated`() {
+        val invalidName = PersonName.newBuilder()
+            .setGivenName("Adam")
+            .buildPartial()
+        val invalidAddress = Address.newBuilder()
+            .setSecondLine("Wall St. 1")
+            .buildPartial()
+        val msg = DeliveryReceiver.newBuilder()
+            .setName(invalidName)
+            .setAddress(invalidAddress)
+            .buildPartial()
+
+        val error = msg.validate()
+
+        error.shouldBePresent()
+
+        val violations = error.get().constraintViolationList
+
+        // We should have 2 violations instead of 1.
+        // But we do not handle the `(required_field)` option.
+        // See https://github.com/SpineEventEngine/validation/issues/148
+        violations.size shouldBe 1
+
+        val receiverViolation = violations[0]
+        receiverViolation!!.fieldPath.getFieldName(0) shouldBe "address"
+
+        val nestedViolations = receiverViolation.violationList
+        nestedViolations.size shouldBe 2
+        nestedViolations[0].fieldPath.getFieldName(0) shouldBe "first_line"
+        nestedViolations[1].fieldPath.getFieldName(0) shouldBe "town"
     }
 
     @Test
     @DisplayName("repeated message fields are validated and violations are stored separately")
-    void validateRepeated() {
-        var msg = DeliveryReceiver.newBuilder()
-                .setName(PersonName.newBuilder()
-                                 .setGivenName("Eve"))
-                .setAddress(Address.newBuilder()
-                                    .setFirstLine("Strand 42")
-                                    .setTown(Town.newBuilder()
-                                                     .setCity("London")
-                                                     .setCountry("UK")
-                                                     .buildPartial()))
-                .addContact(PhoneNumber.getDefaultInstance())
-                .addContact(PhoneNumber.newBuilder()
-                                    .setDigits("not a number")
-                                    .buildPartial())
-                .addContact(PhoneNumber.newBuilder()
-                                    .setDigits("definitely not a number")
-                                    .buildPartial())
-                .buildPartial();
-        var error = msg.validate();
-        assertThat(error)
-                .isPresent();
-        var violations = error.get()
-                              .getConstraintViolationList();
-        assertThat(violations)
-                .hasSize(2);
-        for (var invalidPhone : violations) {
-            assertThat(invalidPhone.getFieldPath()
-                                   .getFieldName(0))
-                    .isEqualTo("contact");
-            assertThat(invalidPhone.getViolationList())
-                    .hasSize(1);
+    fun validateRepeated() {
+        val name = PersonName.newBuilder().setGivenName("Eve")
+        val invalidNumbers = arrayOf(
+            PhoneNumber.getDefaultInstance(),
+            PhoneNumber.newBuilder().setDigits("not a number").buildPartial(),
+            PhoneNumber.newBuilder().setDigits("definitely not a number").buildPartial()
+        )
+        val town = Town.newBuilder()
+            .setName("London")
+            .setCountry("UK")
+        val address = Address.newBuilder()
+            .setFirstLine("Strand 42")
+            .setTown(town)
+        val msg = DeliveryReceiver.newBuilder()
+            .setName(name)
+            .setAddress(address)
+            .addContact(invalidNumbers[0])
+            .addContact(invalidNumbers[1])
+            .addContact(invalidNumbers[2])
+            .buildPartial()
+
+        val error = msg.validate()
+
+        error.shouldBePresent()
+
+        val violations = error.get().constraintViolationList
+        violations.size shouldBe 2
+
+        for (violation in violations) {
+            violation.violationList.size shouldBe 1
+            violation.fieldPath.getFieldName(0) shouldBe "contact"
         }
     }
 
     @Test
     @DisplayName("recursive validation has an exit point")
-    void recursive() {
-        var tree = BinaryTree.newBuilder()
-                .setValue(pack(newUuid()))
-                .setLeftChild(BinaryTree.newBuilder()
-                                      .setValue(pack(newUuid())))
-                .build();
-        var error = tree.validate();
-        assertThat(error).isEmpty();
+    fun recursive() {
+        val nonEmptyAny = Identifier.pack(Identifier.newUuid())
+        val tree = BinaryTree.newBuilder()
+            .setValue(nonEmptyAny)
+            .setLeftChild(
+                BinaryTree.newBuilder()
+                    .setValue(nonEmptyAny)
+            )
+            // We don't have to set the `right_child` field because the field is not `(required)`.
+            .build()
+
+        val error = tree.validate()
+
+        error.shouldBeEmpty()
     }
 
     @Test
-    @DisplayName("not run validation if option value is `false`")
-    void ignoreIfFalse() {
-        var receiver = DeliveryReceiver.newBuilder()
-                .setName(PersonName.newBuilder()
-                                 .setGivenName("Shawn"))
-                .setAddress(Address.newBuilder()
-                                    .setFirstLine("Window St. 2")
-                                    .setTown(Town.newBuilder()
-                                                     .setCity("Kharkiv")
-                                                     .setCountry("Ukraine")))
-                .addEmail(EmailAddress.newBuilder()
-                                  .setValue("definitely not an email")
-                                  .buildPartial())
-                .build();
-        assertThat(receiver.validate())
-                .isEmpty();
+    @DisplayName("not run validation if the option is `false`")
+    fun ignoreIfFalse() {
+        val name = PersonName.newBuilder().setGivenName("Shawn")
+        val town = Town.newBuilder().setName("Oeiras").setCountry("Portugal")
+        val invalidEmail = EmailAddress.newBuilder()
+            .setValue("definitely not an email")
+            .buildPartial()
+        val address = Address.newBuilder().setFirstLine("Window St. 2").setTown(town)
+        val msg = assertDoesNotThrow {
+            // ... because the option `(validate)` is set to `false` on
+            // the `email` field of the `DeliveryReceiver` message.
+            DeliveryReceiver.newBuilder()
+                .setName(name)
+                .setAddress(
+                    address
+                )
+                .addEmail(invalidEmail)
+                .build()
+        }
+
+        msg.validate().shouldBeEmpty()
     }
 }
