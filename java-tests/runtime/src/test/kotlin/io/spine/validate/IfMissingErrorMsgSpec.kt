dd8b8827fb68daf8375a5ba3bd09d85bd99439d0
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2024-11-06 16:31:51 +0000

    Pull up the function to the file level

--- java-tests/runtime/src/test/kotlin/io/spine/validate/IfMissingErrorMsgSpec.kt
+++ java-tests/runtime/src/test/kotlin/io/spine/validate/IfMissingErrorMsgSpec.kt
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Redistribution and use in source and/or binary forms, with or without
  * modification, must retain the above copyright notice and the following
@@ -126,12 +126,12 @@ internal class IfMissingErrorMsgSpec : ValidationOfConstraintTest() {
                 defaultInstance.descriptorForType)
         }
     }
+}
 
-    private fun customErrorMessageFrom(descriptor: Descriptor): String {
-        val firstFieldDescriptor = descriptor.fields[0]
-        return firstFieldDescriptor.options
-            .getExtension(OptionsProto.ifMissing)
-            .errorMsg
-    }
+private fun customErrorMessageFrom(descriptor: Descriptor): String {
+    val firstFieldDescriptor = descriptor.fields[0]
+    return firstFieldDescriptor.options
+        .getExtension(OptionsProto.ifMissing)
+        .errorMsg
 }
 
