1e8ee69806b81995ecd4b82e1d63824b9cf2cbc7
yevhenii-nadtochii <yevhenii.nadtochii@teamdev.com>
2024-11-12 16:47:06 +0100

    Bump ProtoData

--- buildSrc/src/main/kotlin/io/spine/dependency/local/ProtoData.kt
+++ buildSrc/src/main/kotlin/io/spine/dependency/local/ProtoData.kt
@@ -73,7 +73,7 @@ object ProtoData {
      * The version of ProtoData dependencies.
      */
     val version: String
-    private const val fallbackVersion = "0.66.99"
+    private const val fallbackVersion = "0.66.98"
 
     /**
      * The distinct version of ProtoData used by other build tools.
