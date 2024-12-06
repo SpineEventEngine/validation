ff62fa1be1071cfb2121dd3c5d4c4a7d69356108
yevhenii-nadtochii <yevhenii.nadtochii@teamdev.com>
2024-12-06 14:25:16 +0100

    Refactor `GoesPolicy`

--- model/src/main/kotlin/io/spine/validation/GoesPolicy.kt
+++ model/src/main/kotlin/io/spine/validation/GoesPolicy.kt
@@ -31,6 +31,7 @@ import io.spine.base.FieldPath
 import io.spine.core.External
 import io.spine.core.Where
 import io.spine.option.GoesOption
+import io.spine.protodata.ast.Field
 import io.spine.protodata.ast.event.FieldOptionDiscovered
 import io.spine.protodata.plugin.Policy
 import io.spine.protodata.type.resolve
@@ -39,40 +40,67 @@ import io.spine.server.event.Just.Companion.just
 import io.spine.server.event.React
 import io.spine.validation.event.RuleAdded
 
+/**
+ * A policy to add a validation rule to a type whenever the `(goes)` field option
+ * is discovered.
+ *
+ * This option, when being applied to a target field `A`, declares a dependency to
+ * a field `B`. So, whenever `A` is set, `B` also must be set.
+ *
+ * Upon discovering a field with the mentioned option, the police emits the following
+ * composite rule for `A`: `(A isNot Set) OR (B is Set)`.
+ *
+ * Please note, this police relies on implementation of `required` option to determine
+ * whether the field is set. Thus, inheriting its behavior regarding the supported
+ * field types and specification about when a field of a specific type is considered
+ * to be set.
+ */
 internal class GoesPolicy : Policy<FieldOptionDiscovered>() {
+
+    private companion object {
+        const val NO_ERROR_MESSAGE = ""
+    }
+
+    override val typeSystem by lazy { super.typeSystem!! }
 
     @React
     override fun whenever(
         @External @Where(field = OPTION_NAME, equals = GOES)
         event: FieldOptionDiscovered
     ): Just<RuleAdded> {
-        val thisField = event.subject
+        val target = event.subject
         val option = event.option.value.unpack<GoesOption>()
-
-        // TODO:2024-12-03:yevhenii.nadtochii: Create an overload for `resolve` with `TypeName`.
-        val declaringMessage = typeSystem!!.findMessage(thisField.declaringType)!!.first
-
-        val thisFieldShouldBeUnset = RequiredRule.forField(thisField, option.errorMessage())!!
-            .simple.toBuilder()
-            .setOperator(ComparisonOperator.EQUAL)
-            .build()
-            .wrap()
-
+        val declaringMessage = typeSystem.findMessage(target.declaringType)!!.first
         val companionFieldName = FieldPath(option.with)
-        val companionField = typeSystem!!.resolve(companionFieldName, declaringMessage)
-        val companionFieldShouldBeSet = RequiredRule.forField(companionField, "Left side")!!
-
+        val companionField = typeSystem.resolve(companionFieldName, declaringMessage)
         val rule = compositeRule {
-            left = thisFieldShouldBeUnset
+            left = targetFieldShouldBeUnset(target)
             operator = LogicalOperator.OR
-            right = companionFieldShouldBeSet
+            right = companionFieldShouldBeSet(companionField)
             errorMessage = option.errorMessage()
-            field = thisField.name
+            field = target.name
         }.wrap()
-
-        return just(rule.toEvent(thisField.declaringType))
+        return just(rule.toEvent(target.declaringType))
     }
 
+    /**
+     * Creates a simple rule that makes sure the given [target] field is NOT set.
+     */
+    private fun targetFieldShouldBeUnset(target: Field) =
+        RequiredRule.forField(target, NO_ERROR_MESSAGE)!!
+            .simple.toBuilder()
+            .setOperator(ComparisonOperator.EQUAL)
+            .build().wrap()
+
+    /**
+     * Creates a simple rule that makes sure the given [companion] field is SET.
+     */
+    private fun companionFieldShouldBeSet(companion: Field) =
+        RequiredRule.forField(companion, NO_ERROR_MESSAGE)!!
+
+    /**
+     * Returns an error message for the outer composite rule.
+     */
     private fun GoesOption.errorMessage() = errorMsg.ifEmpty { DEFAULT_MESSAGE }
 }
 
