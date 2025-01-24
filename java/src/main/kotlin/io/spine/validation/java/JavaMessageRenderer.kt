cc572809cbb3201957d9cc0915c1b32e4e8604fe
yevhenii-nadtochii <yevhenii.nadtochii@teamdev.com>
2025-01-24 12:43:39 +0100

    Document `MessageValidationCode`

--- java/src/main/kotlin/io/spine/validation/java/JavaMessageRenderer.kt
+++ java/src/main/kotlin/io/spine/validation/java/JavaMessageRenderer.kt
@@ -29,34 +29,16 @@ package io.spine.validation.java
 import com.google.protobuf.Message
 import com.intellij.psi.PsiJavaFile
 import io.spine.protodata.java.file.hasJavaRoot
-import io.spine.protodata.java.javaClassName
 import io.spine.protodata.java.render.JavaRenderer
-import io.spine.protodata.java.render.findClass
 import io.spine.protodata.render.SourceFileSet
 import io.spine.protodata.type.TypeSystem
-import io.spine.tools.psi.java.execute
-import io.spine.validate.NonValidated
-import io.spine.validate.Validated
 import io.spine.validation.CompilationMessage
-import io.spine.validation.java.psi.nested
 
 /**
  * The main Java renderer of the validation library.
  *
- * This rendered is applied to every compiled [Message], even if the message does not
- * have any declared constraints.
- *
- * In particular, the renderer does the following:
- *
- * 1. Makes [Message] implement [io.spine.validate.ValidatableMessage] interface.
- * 2. Declares `validate()` method in [Message] containing the constraints, if any.
- * 3. Declares supporting fields and methods in [Message], if any.
- * 4. Inserts invocation of `validate()` into [Message.Builder.build] method.
- *
- * Also, it puts the following annotations:
- *
- * 1. [Validated] for [Message.Builder.build] method.
- * 2. [NonValidated] for [Message.Builder.buildPartial] method.
+ * This rendered is applied to every [Message] being compiled, even if the message
+ * does not have any declared constraints.
  */
 public class JavaMessageRenderer : JavaRenderer() {
 
@@ -73,27 +55,13 @@ public class JavaMessageRenderer : JavaRenderer() {
             return
         }
 
-        val allCompilationMessages = select(CompilationMessage::class.java).all()
+        select(CompilationMessage::class.java).all()
             .associateWith { sources.javaFileOf(it.type) }
-
-        // Adds `implements ValidatableMessage`, `validate()` and supporting members.
-        allCompilationMessages.forEach { (message, file) ->
-            val psiFile = file.psi() as PsiJavaFile
-
-            val messageCode = MessageValidationCode(renderer = this, message)
-            val messageClassName = message.type.javaClassName(typeSystem)
-            val messageClass = psiFile.findClass(messageClassName)
-            execute {
-                messageCode.render(messageClass)
+            .forEach { (message, file) ->
+                val messageCode = MessageValidationCode(renderer = this, message)
+                val psiFile = file.psi() as PsiJavaFile
+                messageCode.render(psiFile)
+                file.overwrite(psiFile.text)
             }
-
-            val builderCode = BuilderValidationCode(messageClassName)
-            val builderClass = messageClass.nested("Builder")
-            execute {
-                builderCode.render(builderClass)
-            }
-
-            file.overwrite(psiFile.text)
-        }
     }
 }
--- java/src/main/kotlin/io/spine/validation/java/MessageValidationCode.kt
+++ java/src/main/kotlin/io/spine/validation/java/MessageValidationCode.kt
@@ -27,107 +27,98 @@
 package io.spine.validation.java
 
 import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiJavaFile
 import com.squareup.javapoet.CodeBlock
 import com.squareup.javapoet.FieldSpec
 import com.squareup.javapoet.MethodSpec
 import io.spine.protodata.ast.TypeName
 import io.spine.protodata.java.ReadVar
 import io.spine.protodata.java.This
+import io.spine.protodata.java.javaClassName
+import io.spine.protodata.java.render.findClass
 import io.spine.tools.psi.java.Environment.elementFactory
 import io.spine.tools.psi.java.addLast
 import io.spine.tools.psi.java.annotate
 import io.spine.tools.psi.java.createInterfaceReference
+import io.spine.tools.psi.java.execute
 import io.spine.tools.psi.java.implement
+import io.spine.tools.psi.java.method
 import io.spine.validate.ConstraintViolation
+import io.spine.validate.NonValidated
 import io.spine.validate.ValidatableMessage
+import io.spine.validate.Validated
+import io.spine.validate.ValidatingBuilder
 import io.spine.validation.CompilationMessage
 import io.spine.validation.Rule
+import io.spine.validation.java.psi.addBefore
+import io.spine.validation.java.psi.createStatementsFromText
+import io.spine.validation.java.psi.deepSearch
+import io.spine.validation.java.psi.nested
+
+private typealias MessagePsiClass = PsiClass
+private typealias BuilderPsiClass = PsiClass
 
 /**
  * Generates validation code for the given [CompilationMessage].
  *
- * Serves as a method object for the [JavaMessageRenderer] passed
- * to the constructor.
+ * This class modifies both the generated Java class of the message and its builder.
  *
- * In particular, this class does the following:
+ * All validation logic is injected into the [ValidatableMessage.validate] method
+ * within the message class itself. This allows a message instance to be validated even
+ * after it has been built. Note that this method does not throw exceptions directly;
+ * instead, it returns any detected violations, if any.
  *
- * 1. Makes the [message] implement [ValidatableMessage] interface.
- * 2. Adds implementation of [ValidatableMessage.validate] method.
- * 3. Declares supporting members, if any.
+ * The message builder is modified to invoke the [ValidatableMessage.validate] just
+ * before returning from its [build][com.google.protobuf.Message.Builder.build] method.
+ * If one or more violations are detected, the builder will throw an exception.
  */
+@Suppress("TooManyFunctions") // Small methods representing atomic PSI modifications.
 internal class MessageValidationCode(
     private val renderer: JavaMessageRenderer,
     private val message: CompilationMessage,
 ) {
     private val messageType: TypeName = message.name
-    private val constraints = mutableListOf<CodeBlock>()
+    private val messageClassName = messageType.javaClassName(renderer.typeSystem)
     private val supportingFields = mutableListOf<FieldSpec>()
     private val supportingMethods = mutableListOf<MethodSpec>()
+    private val constraints = mutableListOf<CodeBlock>()
 
-    fun render(messageClass: PsiClass) {
-        message.ruleList.forEach(::generateRule)
-        with(messageClass) {
-            implementInterface()
-            implementMethod()
-            declareSupportingFields()
-            declareSupportingMethods()
+    /**
+     * Renders the message validation code into the provided [psiFile].
+     *
+     * @param psiFile A source file with the generated Java class for the [message].
+     *
+     * @throws IllegalStateException if [psiFile] doesn't contain a Java class for the [message].
+     */
+    fun render(psiFile: PsiJavaFile) {
+        message.ruleList.forEach(::generate)
+        val messageClass = psiFile.findClass(messageClassName)
+        val builderClass = messageClass.nested("Builder")
+        execute {
+            messageClass.apply {
+                implementValidatableMessage()
+                declareValidateMethod()
+                declareSupportingFields()
+                declareSupportingMethods()
+            }
+            builderClass.apply {
+                implementValidatingBuilder()
+                injectValidation()
+                annotateBuildReturnType()
+                annotateBuildPartialReturnType()
+            }
         }
     }
 
-    private fun PsiClass.implementInterface() {
-        val qualifiedName = ValidatableMessage::class.java.canonicalName
-        val reference = elementFactory.createInterfaceReference(qualifiedName)
-        implement(reference)
-    }
-
-    private fun PsiClass.implementMethod() {
-        val psiMethod = elementFactory.createMethodFromText(
-            """
-            |public java.util.Optional<io.spine.validate.ValidationError> validate() {
-            |    ${methodBody()}
-            |}
-            """.trimMargin(), this
-        )
-        psiMethod.annotate(Override::class.java)
-        addLast(psiMethod)
-    }
-
-    private fun methodBody(): String {
-        if (constraints.isEmpty()) {
-            return "return java.util.Optional.empty();"
-        }
-
-        // `CodeBlock` is printed with a new line in the end, so no need to call `joinByLine()`.
-        // We are trimming because we don't need a trailing empty line.
-        val formattedConstraints = constraints.joinToString(separator = "").trim()
-
-        return """
-        var $violations = new java.util.ArrayList<io.spine.validate.ConstraintViolation>();
-        
-        $formattedConstraints
-        
-        if (!$violations.isEmpty()) {
-            var error = io.spine.validate.ValidationError.newBuilder()
-                .addAllConstraintViolation($violations)
-                .build();
-            return java.util.Optional.of(error);
-        } else {
-            return java.util.Optional.empty();
-        }
-        """.trimIndent()
-    }
-
-    private fun PsiClass.declareSupportingFields() =
-        supportingFields.forEach {
-            addLast(elementFactory.createFieldFromText(it.toString(), this))
-        }
-
-    private fun PsiClass.declareSupportingMethods() =
-        supportingMethods.forEach {
-            addLast(elementFactory.createMethodFromText(it.toString(), this))
-        }
-
-    private fun generateRule(rule: Rule) {
+    /**
+     * Generates a Java [constraint][constraints], [supportingFields] and [supportingMethods]
+     * for the passed [rule].
+     *
+     * A single rule always generates a single Java constraint represented by a [CodeBlock].
+     * The number of supported members is not restricted. A single rule may generate zero,
+     * one, or more supporting members.
+     */
+    private fun generate(rule: Rule) {
         val context = newContext(rule, message)
         val generator = generatorFor(context)
         constraints.add(generator.code())
@@ -146,7 +137,90 @@ internal class MessageValidationCode(
             violationList = violations
         )
 
-    companion object {
-        private val violations = ReadVar<MutableList<ConstraintViolation>>("violations")
+    private fun MessagePsiClass.implementValidatableMessage() {
+        val qualifiedName = ValidatableMessage::class.java.canonicalName
+        val reference = elementFactory.createInterfaceReference(qualifiedName)
+        implement(reference)
+    }
+
+    private fun MessagePsiClass.declareValidateMethod() {
+        // `CodeBlock` is printed with a new line in the end, so no need to call `joinByLine()`.
+        // And we have to trim because we don't need a trailing empty line.
+        val formattedConstraints = constraints.joinToString(separator = "").trim()
+        val psiMethod = elementFactory.createMethodFromText(
+            """
+            |public java.util.Optional<io.spine.validate.ValidationError> validate() {
+            |    ${validateMethodBody(formattedConstraints)}
+            |}
+            """.trimMargin(), this
+        )
+        psiMethod.annotate(Override::class.java)
+        addLast(psiMethod)
+    }
+
+    private fun validateMethodBody(formattedConstraints: String): String =
+        if (formattedConstraints.isEmpty()) {
+            "return java.util.Optional.empty();"
+        } else {
+            """
+            var $violations = new java.util.ArrayList<io.spine.validate.ConstraintViolation>();
+            $formattedConstraints
+            if (!$violations.isEmpty()) {
+                var error = io.spine.validate.ValidationError.newBuilder()
+                    .addAllConstraintViolation($violations)
+                    .build();
+                return java.util.Optional.of(error);
+            } else {
+                return java.util.Optional.empty();
+            }
+            """.trimIndent()
+        }
+
+    private fun MessagePsiClass.declareSupportingFields() =
+        supportingFields.forEach {
+            addLast(elementFactory.createFieldFromText(it.toString(), this))
+        }
+
+    private fun MessagePsiClass.declareSupportingMethods() =
+        supportingMethods.forEach {
+            addLast(elementFactory.createMethodFromText(it.toString(), this))
+        }
+
+    private fun BuilderPsiClass.implementValidatingBuilder() {
+        val qualifiedName = ValidatingBuilder::class.java.canonicalName
+        val genericParameter = messageClassName.canonical
+        val reference = elementFactory.createInterfaceReference(qualifiedName, genericParameter)
+        implement(reference)
+    }
+
+    /**
+     * Injects an invocation of [ValidatableMessage.validate] method into the end
+     * of `build()` method body of this [PsiClass].
+     *
+     * The validation code is executed right before returning from the `build()`.
+     * If one or more constraints are violated, the injected snippet will throw.
+     */
+    private fun BuilderPsiClass.injectValidation() = method("build").run {
+        val returningResult = deepSearch("return result;")
+        val runValidation = elementFactory.createStatementsFromText(
+            """
+            java.util.Optional<io.spine.validate.ValidationError> error = result.validate();
+            if (error.isPresent()) {
+                var violations = error.get().getConstraintViolationList();
+                throw new io.spine.validate.ValidationException(violations);
+            }
+            """.trimIndent(), this
+        )
+        body!!.addBefore(runValidation, returningResult)
+    }
+
+    private fun BuilderPsiClass.annotateBuildReturnType() = method("build")
+        .run { returnTypeElement!!.addAnnotation(Validated::class.qualifiedName!!) }
+
+    private fun BuilderPsiClass.annotateBuildPartialReturnType() = method("buildPartial")
+        .run { returnTypeElement!!.addAnnotation(NonValidated::class.qualifiedName!!) }
+
+    private companion object {
+        val violations = ReadVar<MutableList<ConstraintViolation>>("violations")
     }
 }
