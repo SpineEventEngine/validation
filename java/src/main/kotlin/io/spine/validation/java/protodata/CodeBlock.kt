830ad672be3da1f422a481ccf07d534a435263e7
yevhenii-nadtochii <yevhenii.nadtochii@teamdev.com>
2025-01-30 15:54:58 +0100

    Introduce `CodeBlock` for Expression API

---
+++ java/src/main/kotlin/io/spine/validation/java/protodata/CodeBlock.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2025, TeamDev. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.validation.java.protodata
+
+import io.spine.protodata.java.AnElement
+import io.spine.protodata.java.JavaElement
+
+/**
+ * An arbitrary piece of Java code that can be thought as a collection
+ * of one or more [JavaElement]s.
+ *
+ * @param code Arbitrary Java code.
+ */
+public class CodeBlock(public val code: String) : JavaElement {
+
+    override fun toCode(): String = code
+
+    override fun equals(other: Any?): Boolean =
+        other is AnElement && this.code == other.code
+
+    override fun hashCode(): Int = code.hashCode()
+
+    override fun toString(): String = code
+}
