e38ae3ee372bf5bab0a4c35d56a5092bae3e636c
yevhenii-nadtochii <yevhenii.nadtochii@teamdev.com>
2025-02-10 18:34:13 +0100

    Fix the JVM name

--- java/src/main/kotlin/io/spine/validation/java/ClassNames.kt
+++ java/src/main/kotlin/io/spine/validation/java/ClassNames.kt
@@ -24,7 +24,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-@file:JvmName("ConstraintViolations")
+@file:JvmName("ClassNames")
 
 package io.spine.validation.java
 
