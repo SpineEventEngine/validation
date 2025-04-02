1622185421230ccb439ea120abef0eafe670faf4
yevhenii-nadtochii <yevhenii.nadtochii@teamdev.com>
2025-04-02 15:55:57 +0200

    Rename `ValidationPluginPart` to `WithValidationSettings`

--- model/src/main/kotlin/io/spine/validation/WithValidationSettings.kt
+++ model/src/main/kotlin/io/spine/validation/WithValidationSettings.kt
@@ -33,7 +33,7 @@ import io.spine.protodata.settings.defaultConsumerId
  * An interface common for components of [ValidationPlugin] that load settings
  * passed as a file containing [ValidationConfig] instance written using the plugin class name.
  */
-public interface ValidationPluginPart: LoadsSettings {
+public interface WithValidationSettings: LoadsSettings {
 
     override val consumerId: String
         get() = ValidationPlugin::class.java.defaultConsumerId
