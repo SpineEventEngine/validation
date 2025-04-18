f93a5c173b5626999a64e3f5f72396c48c559f69
yevhenii-nadtochii <yevhenii.nadtochii@teamdev.com>
2025-04-18 15:45:41 +0200

    Enable `RequireMessageGenerator`

--- java/src/main/kotlin/io/spine/validation/java/JavaValidationRenderer.kt
+++ java/src/main/kotlin/io/spine/validation/java/JavaValidationRenderer.kt
@@ -47,6 +47,7 @@ import io.spine.validation.java.generate.option.MinGenerator
 import io.spine.validation.java.generate.option.PatternGenerator
 import io.spine.validation.java.generate.option.RangeGenerator
 import io.spine.validation.java.generate.option.ChoiceGenerator
+import io.spine.validation.java.generate.option.RequireGenerator
 import io.spine.validation.java.generate.option.RequiredGenerator
 import io.spine.validation.java.generate.option.ValidateGenerator
 import io.spine.validation.java.generate.option.WhenGenerator
@@ -76,6 +77,7 @@ public class JavaValidationRenderer : JavaRenderer() {
             MinGenerator(querying),
             ChoiceGenerator(querying),
             WhenGenerator(querying, valueConverter),
+            RequireGenerator(querying, valueConverter),
         )
     }
 
