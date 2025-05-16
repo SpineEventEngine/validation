9c770a026b13458dc5e271eb57bad14d4df30a7d
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2025-05-16 16:39:32 +0100

    Improve doc layout

--- java-runtime/src/main/java/io/spine/validate/option/StandardOptionFactory.java
+++ java-runtime/src/main/java/io/spine/validate/option/StandardOptionFactory.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2022, TeamDev. All rights reserved.
+ * Copyright 2025, TeamDev. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Redistribution and use in source and/or binary forms, with or without
  * modification, must retain the above copyright notice and the following
@@ -32,9 +32,10 @@ import io.spine.annotation.Internal;
 /**
  * A {@link ValidatingOptionFactory} of known options.
  *
- * <p>The options produced by a {@code StandardOptionFactory} are supported by the code validation
- * generation. If code generation takes place, checking constraints produced by these options is
- * obsolete.
+ * <p>The options produced by a {@code StandardOptionFactory} are supported
+ * by the code validation generation.
+ *
+ * <p>If code generation takes place, checking constraints produced by these options is obsolete.
  */
 @Internal
 @Immutable
