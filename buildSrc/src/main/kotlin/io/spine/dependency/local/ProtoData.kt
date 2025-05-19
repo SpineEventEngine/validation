e609333985c4c1a06b1068d963e284a07837b4af
yevhenii-nadtochii <yevhenii.nadtochii@teamdev.com>
2025-05-19 14:22:53 +0200

    Take the latest `ProtoData`

--- buildSrc/src/main/kotlin/io/spine/dependency/local/ProtoData.kt
+++ buildSrc/src/main/kotlin/io/spine/dependency/local/ProtoData.kt
@@ -72,7 +72,7 @@ object ProtoData {
      * The version of ProtoData dependencies.
      */
     val version: String
-    private const val fallbackVersion = "0.96.3"
+    private const val fallbackVersion = "0.96.4"
 
     /**
      * The distinct version of ProtoData used by other build tools.
