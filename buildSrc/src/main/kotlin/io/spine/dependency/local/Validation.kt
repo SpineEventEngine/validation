03e91ecf0d592c1a6af01ff59411195b01ae90d2
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2025-12-25 17:44:53 +0000

    Bump internal dependencies

--- buildSrc/src/main/kotlin/io/spine/dependency/local/Validation.kt
+++ buildSrc/src/main/kotlin/io/spine/dependency/local/Validation.kt
@@ -36,7 +36,7 @@ object Validation {
     /**
      * The version of the Validation library artifacts.
      */
-    const val version = "2.0.0-SNAPSHOT.383"
+    const val version = "2.0.0-SNAPSHOT.390"
 
     /**
      * The last version of Validation compatible with ProtoData.
