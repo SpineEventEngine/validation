0e9cc873998a7c6eb87db223bad50f2e67ad95a6
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2026-02-03 21:19:20 +0000

    Add Gradle project

---
+++ docs/build.gradle.kts
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2026, TeamDev. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * Builds and runs the site locally.
+ */
+task<Exec>("runSite") {
+    dependsOn("installDependencies")
+    commandLine("./_script/hugo-serve")
+}
+
+/**
+ * Builds the site without starting the server.
+ */
+task<Exec>("buildSite") {
+    dependsOn("installDependencies")
+    commandLine("./_script/hugo-build")
+}
+
+/**
+ * Installs the Node.js dependencies required for building the site.
+ */
+task<Exec>("installDependencies") {
+    commandLine("./_script/install-dependencies")
+}
+
+/**
+ * Embeds the code samples into pages of the site.
+ */
+task<Exec>("embedCode") {
+    commandLine("./_script/embed-code")
+}
+
+/**
+ * Verifies that the source code samples embedded into the pages are up-to-date.
+ */
+task<Exec>("checkSamples") {
+    commandLine("./_script/check-samples")
+}
+
+/**
+ * Builds all included projects via depending on the top-level "buildAll" tasks
+ * declared in these projects.
+ *
+ * See also:
+ *  * [Composite build to build subprojects](https://discuss.gradle.org/t/defining-a-composite-build-only-to-build-all-subprojects/25070/6)
+ *  * [Gradlew composite build example](https://github.com/AlexMAS/gradle-composite-build-example)
+ *  * [Composite builds](https://docs.gradle.org/current/userguide/composite_builds.html)
+ */
+tasks.register("buildAll") {
+    dependsOn(gradle.includedBuilds.map { it.task(":buildAll") })
+}
