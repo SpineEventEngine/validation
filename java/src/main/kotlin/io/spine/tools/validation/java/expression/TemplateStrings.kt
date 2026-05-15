3a08b4d7b95db0e62166b8482ce7a59d77e3de81
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2026-05-15 22:50:09 +0100

    Migrate to `Placeholder` API

--- java/src/main/kotlin/io/spine/tools/validation/java/expression/TemplateStrings.kt
+++ java/src/main/kotlin/io/spine/tools/validation/java/expression/TemplateStrings.kt
@@ -35,6 +35,7 @@ import io.spine.tools.compiler.jvm.mapExpression
 import io.spine.tools.compiler.jvm.newBuilder
 import io.spine.string.Placeholder
 import io.spine.string.TemplateString
+import io.spine.string.joinQuoted
 
 /**
  * Yields an expression that creates a new instance of [TemplateString].
@@ -50,29 +51,17 @@ public fun templateString(
     template: String,
     placeholders: Map<Placeholder, Expression<String>>,
     optionName: String
-): Expression<TemplateString> =
-    withStringPlaceholders(template, placeholders.mapKeys { it.key.name }, optionName)
-
-/**
- * Yields an expression that creates a new instance of [TemplateString].
- *
- * @param placeholders The supported placeholders and their values.
- * @param optionName The name of the option, which declared the provided [placeholders].
- */
-public fun withStringPlaceholders(
-    template: String,
-    placeholders: Map<String, Expression<String>>,
-    optionName: String
 ): Expression<TemplateString> {
-    checkPlaceholdersHasValue(template, placeholders) { missingKeys ->
-        "Unexpected error message placeholders `$missingKeys` specified for the `($optionName)`" +
-                " option. The available placeholders: `${placeholders.keys}`. Please make sure" +
-                " that the code that verifies the message placeholders and its code generator" +
-                " operate with the same set of placeholders."
+    checkPlaceholdersHasValue(template, placeholders.keys) { missing ->
+        "Unexpected error message placeholders ${missing.joinQuoted()} specified for" +
+                " the `($optionName)` option." +
+                " The available placeholders: ${placeholders.keys.joinQuoted()}." +
+                " Please make sure that the code that verifies the message placeholders and" +
+                " its code generator operate with the same set of placeholders."
     }
     val placeholderEntries = mapExpression(
         StringClass, StringClass,
-        placeholders.mapKeys { StringLiteral(it.key) }
+        placeholders.mapKeys { StringLiteral(it.key.name) }
     )
     val escapedTemplate = restoreProtobufEscapes(template)
     return TemplateStringClass.newBuilder()
@@ -86,49 +75,16 @@ public fun withStringPlaceholders(
  * in the [placeholders] set.
  *
  * @param template The template with placeholders like `${something}`.
- * @param placeholders The list with placeholder values.
- * @param lazyMessage The message to use in [IllegalArgumentException] if the check fails.
+ * @param placeholders The set of available placeholders.
  */
 private fun checkPlaceholdersHasValue(
     template: String,
-    placeholders: Set<String>,
-    lazyMessage: (List<String>) -> String =
-        { "Missing value for the following template placeholders: `$it`." }
+    placeholders: Set<Placeholder>,
+    lazyMessage: (List<Placeholder>) -> String
 ) {
-    val neededPlaceholders = extractPlaceholders(template)
-    val missing = mutableListOf<String>()
-    for (placeholder in neededPlaceholders) {
-        if (!placeholders.contains(placeholder)) {
-            missing.add(placeholder)
-        }
-    }
+    val needed = Placeholder.extractPlaceholders(template)
+    val missing = needed.filter { it !in placeholders }
     if (missing.isNotEmpty()) {
         throw IllegalArgumentException(lazyMessage(missing))
     }
 }
-
-/**
- * Makes sure that each placeholder within the [template] string has a value
- * in [placeholders] map.
- *
- * @param template The template with placeholders like `${something}`.
- * @param placeholders The map containing placeholders (without curly braces and the dollar sign)
- *  and their values.
- * @param lazyMessage The message to use in [IllegalArgumentException] if the check fails.
- */
-private fun checkPlaceholdersHasValue(
-    template: String,
-    placeholders: Map<String, Any>,
-    lazyMessage: (List<String>) -> String =
-        { "Missing value for the following template placeholders: `$it`." }
-): Unit = checkPlaceholdersHasValue(template, placeholders.keys, lazyMessage)
-
-/**
- * Extracts all placeholders used within this [template] string.
- */
-private fun extractPlaceholders(template: String): Set<String> =
-    PLACEHOLDERS.findAll(template)
-        .map { it.groupValues[1] }
-        .toSet()
-
-private val PLACEHOLDERS = Regex("\\$\\{([^}]+)}")
